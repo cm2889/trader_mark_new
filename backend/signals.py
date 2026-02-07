1ac4ed67390cbcec1d836d8da4358526400c6c2f
ASIF <f.asif.official@gmail.com>
2026-02-07 17:10:22 +0600

    refactor: Remove vehicle assignment update logic from VehicleHandover signal

--- backend/signals.py
+++ backend/signals.py
@@ -12,10 +12,6 @@ def sync_vehicle_on_handover(sender, instance, **kwargs):
 	# Mark all other handovers for this vehicle as inactive
 	VehicleHandover.objects.filter(vehicle=instance.vehicle).exclude(id=instance.id).update(is_active=False)
 
-	# Move the vehicle assignment to the new employee, if provided
-	if instance.to_employee and instance.vehicle.employee_id != instance.to_employee_id:
-		Vehicle.objects.filter(id=instance.vehicle_id).update(employee=instance.to_employee)
-
 
 @receiver(post_save, sender=Passport)
 def ensure_single_active_passport(sender, instance, **kwargs):
